b6d18b11c0b83ba17a0ffb915f046db9756bc741
Zeamanuel-Admasu <149781558+Zeamanuel-Admasu@users.noreply.github.com>
2025-02-28 18:41:05 +0300

    Created 'B - Jo's Adventure' by A2SV Hub

---
+++ A2SV G6 - Round #4 28-Feb-2025/B - Jo's Adventure 287003.py
@@ -0,0 +1,27 @@
+# Problem: B - Jo's Adventure - https://codeforces.com/gym/590053/problem/B
+
+a, b = map(int, input().split())
+arr = list(map(int, input().split()))
+arr1 = list(arr[::-1])
+pref = [0] * len(arr)
+for i in range(1, len(arr)):
+    if arr[i] < arr[i-1]:
+        pref[i] = pref[i-1] + abs(arr[i] - arr[i-1])
+    else:
+        pref[i] = pref[i-1]
+pref2 = [0] * len(arr)
+for i in range(1, len(arr1)):
+    if arr1[i] < arr1[i-1]:
+        pref2[i] = pref2[i-1] + abs(arr1[i] - arr1[i-1])
+    else:
+        pref2[i] = pref2[i-1]
+pref2 = pref2[::-1]
+for i in range(b):
+    n, m = map(int, input().split()) 
+    if n < m:
+        print(pref[m-1] - pref[n-1])
+    else: 
+        print( abs(pref2[m-1] - pref2[n-1]))
+        
+            
+
