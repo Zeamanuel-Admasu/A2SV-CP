eb39532eec2a650ef490d873f2efa857a00dd410
Zeamanuel-Admasu <149781558+Zeamanuel-Admasu@users.noreply.github.com>
2025-03-17 22:30:25 +0300

    Created 'Books' by A2SV Hub

---
+++ 17-Mar-2025/Books 309703.py
@@ -0,0 +1,16 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n, t = list(map(int, input().split()))
+nums = list(map(int, input().split()))
+
+j = 0
+maxim = 0
+curr = 0
+
+for i in range(n): 
+    while j < n and curr + nums[j] <= t: 
+        curr += nums[j]
+        j += 1
+    maxim = max(maxim, j - i) 
+    curr -= nums[i] 
+print(maxim)
