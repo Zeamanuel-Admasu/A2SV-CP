f51c6b38f68f36b029cd0aba2ba44ecf6c897c0e
Zeamanuel-Admasu <149781558+Zeamanuel-Admasu@users.noreply.github.com>
2025-03-23 14:50:08 +0300

    Created 'Array Splitting' by A2SV Hub

---
+++ 23-Mar-2025/Array Splitting 316030.py
@@ -0,0 +1,19 @@
+# Problem: Array Splitting - https://codeforces.com/problemset/problem/1197/C
+
+n, k = list(map(int, input().split()))
+arr = list(map(int, input().split()))
+
+leftOver = []
+for i in range(1, len(arr)):
+    leftOver.append(arr[i] - arr[i-1])
+
+L = sorted(leftOver)
+a = L[:len(L) - k + 1]
+print(sum(a))
+# maxim = max(leftOver)
+# minim = min(leftOver)
+# cost = maxim - minim
+# print(cost)
+
+
+
