e18b0e037073c0add1bbebc2235077a9f8fc024e
Zeamanuel-Admasu <149781558+Zeamanuel-Admasu@users.noreply.github.com>
2025-04-17 05:28:44 +0300

    Created 'Rotting Oranges' by A2SV Hub

---
+++ 17-Apr-2025/Rotting Oranges 335942.py
@@ -0,0 +1,28 @@
+# Problem: Rotting Oranges - https://leetcode.com/problems/rotting-oranges/
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        directions = [[0,1], [1, 0], [0, -1], [-1, 0]]
+        rows, cols = len(grid), len(grid[0])
+        time, fresh = 0, 0
+        q = deque()
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    fresh += 1
+                elif grid[i][j] == 2:
+                    q.append([i, j])
+        while q and fresh > 0:
+            for i in range(len(q)):
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    row, col = dr + r, dc + c
+                    if (row < 0 or row == len(grid)) or (col < 0 or col == len(grid[0])) or (grid[row][col] != 1):
+                        continue
+                    grid[row][col] = 2
+                    q.append([row, col])
+                    fresh -= 1
+            time += 1
+        return time if fresh == 0 else -1
+
+
