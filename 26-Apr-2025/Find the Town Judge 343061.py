7b778e9186d87db2285a675254516939bb9207e0
Zeamanuel-Admasu <149781558+Zeamanuel-Admasu@users.noreply.github.com>
2025-04-26 21:37:13 +0300

    Created 'Find the Town Judge' by A2SV Hub

---
+++ 26-Apr-2025/Find the Town Judge 343061.py
@@ -0,0 +1,16 @@
+# Problem: Find the Town Judge - https://leetcode.com/problems/find-the-town-judge/
+
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        incoming = defaultdict(int)
+        outgoing = defaultdict(int)
+
+        for src, dst in trust:
+            outgoing[src] += 1
+            incoming[dst] += 1
+        
+        for i in range(1, n + 1):
+            if outgoing[i] == 0 and incoming[i] == n - 1:
+                return i
+        return -1
+        
