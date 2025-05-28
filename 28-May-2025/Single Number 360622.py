80b8c540c7523ebcdb4fdf19203202b7fc7b9c0f
Zeamanuel-Admasu <149781558+Zeamanuel-Admasu@users.noreply.github.com>
2025-05-28 23:42:47 +0300

    Created 'Single Number' by A2SV Hub

---
+++ 28-May-2025/Single Number 360622.py
@@ -0,0 +1,14 @@
+# Problem: Single Number - https://leetcode.com/problems/single-number/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        i = 0
+        while i < len(nums) - 1:
+            if nums[i] == nums[i + 1]:
+                i += 2
+            else:
+                return nums[i]
+        return nums[-1]
+
+        
